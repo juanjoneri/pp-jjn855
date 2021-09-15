04249142bfbff572199a8eadd4f8768ae4cb78e2
Juan Neri <juanjo.neri@gmail.com>
2021-09-15 16:04:03 -0500

    refactor CustomVisitor to allow more functionality

--- lexparse/code/CustomVisitor.java
+++ lexparse/code/CustomVisitor.java
@@ -12,21 +12,28 @@ public class CustomVisitor extends SimpleLangBaseVisitor<Void> {
         this.hasMain = false;
     }
 
+    /**
+     * Check that this program has a main method with no arguments and return type of void
+     */
     public boolean hasMain() {
         return hasMain;
     }
 
     @Override
     public Void visitMethodDecl(SimpleLangParser.MethodDeclContext ctx) {
-        
+        if (isMain(ctx)) {
+            hasMain = true;
+        }
+
+        return super.visitMethodDecl(ctx);
+    }
+
+    private static boolean isMain(SimpleLangParser.MethodDeclContext ctx) {
         boolean isMain = ctx.ID().getText().equals("main");
         boolean returnsVoid = ctx.VOID() != null;
         boolean hasNoArguments = ctx.formParams() == null;
-
-        if (isMain && returnsVoid && hasNoArguments) {
-            hasMain = true;
-        }
-        return super.visitMethodDecl(ctx);
+        
+        return (isMain && returnsVoid && hasNoArguments);
     }
 
 }
