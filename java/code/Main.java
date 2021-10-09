6d68d2dc4b9ea48e58aced57091b54c3708537bb
Juan Neri <juanjo.neri@gmail.com>
2021-10-08 22:17:43 -0500

    add assumptions file

--- java/code/Main.java
+++ java/code/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         List<List<String>> data = Arrays.asList(
             Arrays.asList("A",                    "B",                   "C"), 
-            Arrays.asList("-11",                  "__to_lower__([0,2])", "__to_upper__([2,2])"), 
+            Arrays.asList("01.0000",                  "__to_lower__([0,2])", "__to_upper__([2,2])"), 
             Arrays.asList("1",                    "22",                  "__avg__([1,0],[0,0])"), 
             Arrays.asList("__sum__([0,0],[0,0])", "jamonNNNN",           "alOha"));
 
